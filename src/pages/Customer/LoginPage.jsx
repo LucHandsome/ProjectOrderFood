e609dfd6426da2ce0fb986904e57654cfe52b18c
LucHandsome <96497540+LucHandsome@users.noreply.github.com>
2024-09-26 17:13:55 +0700

    Cập nhật FE

--- src/pages/Customer/LoginPage.jsx
+++ src/pages/Customer/LoginPage.jsx
@@ -42,34 +42,35 @@ const LoginPage = () => {
     };
 
     // Hàm xử lý nhận mã xác thực và gọi API đăng nhập SSO
-    // useEffect(() => {
-    //     const fetchPointerToken = async () => {
-    //         const urlParams = new URLSearchParams(window.location.search);
-    //         const code = urlParams.get('code');
-
-    //         if (code) {
-    //             setLoading(true);
-    //             try {
-    //                 const response = await axios.post('https://order-app-88-037717b27b20.herokuapp.com/api/customers/sign-in-sso', { code });
-
-    //                 if (response.data.token) {
-    //                     toast.success('Đăng nhập Pointer thành công!');
-    //                     localStorage.setItem('token', response.data.token);
-    //                     localStorage.setItem('customerId', response.data.data.id);
-    //                     navigate('/restaurantlist');
-    //                 } else {
-    //                     toast.error(response.data.message || 'Đăng nhập Pointer không thành công.');
-    //                 }
-    //             } catch (error) {
-    //                 toast.error('Đã xảy ra lỗi khi đăng nhập với Pointer. Vui lòng thử lại.');
-    //             } finally {
-    //                 setLoading(false);
-    //             }
-    //         }
-    //     };
-
-    //     fetchPointerToken();
-    // }, [navigate]);
+    useEffect(() => {
+        const fetchPointerToken = async () => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const code = urlParams.get('code');
+    
+            if (code) {
+                setLoading(true);
+                try {
+                    const response = await axios.post('https://order-app-88-037717b27b20.herokuapp.com/api/customers/sign-in-sso', { code });
+    
+                    if (response.status === 200) {
+                        toast.success('Đăng nhập Pointer thành công!');
+                        localStorage.setItem('token', response.data.token);
+                        localStorage.setItem('customerId', response.data.data.id);
+                        navigate('/restaurantlist'); // Điều hướng đến trang mà không có mã code
+                    } else {
+                        toast.error(response.data.message || 'Đăng nhập Pointer không thành công.');
+                    }
+                } catch (error) {
+                    toast.error('Đã xảy ra lỗi khi đăng nhập với Pointer. Vui lòng thử lại.');
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+    
+        fetchPointerToken();
+    }, [navigate]);
+    
 
     return (
         <div className="flex flex-col md:flex-row h-screen">
